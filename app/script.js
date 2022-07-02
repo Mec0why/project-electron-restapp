da6748c85faa38fbbaf4f7c39f5b16aa9bd7bb00
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-02 22:15:16 +0200

    Add conditional Rendering

--- app/script.js
+++ app/script.js
@@ -5,9 +5,9 @@ class App extends React.Component {
   state = {
     timerOn: false,
     timerStart: 4000,
-    timerTime: 2000, // 1200000 for 20 min. 20000 for 20 sec.
+    timerTime: 4000, // 1200000 for 20 min. 20000 for 20 sec.
     timerRest: false,
-    timerRestTime: 21000,
+    timerRestTime: 20000,
   };
 
   startTimer = () => {
@@ -42,19 +42,15 @@ class App extends React.Component {
 
   stopTimer = () => {
     clearInterval(this.timer);
-    this.setState({ timerOn: false });
-  };
-
-  resetTimer = () => {
-    if (this.state.timerOn === false) {
-      this.setState({
-        timerTime: this.state.timerStart,
-      });
-    }
+    this.setState({
+      timerOn: false,
+      timerRest: false,
+      timerTime: this.state.timerStart,
+    });
   };
 
   render() {
-    const { timerTime, timerOn } = this.state;
+    const { timerTime, timerOn, timerRest } = this.state;
 
     let minutes = ('0' + Math.floor((timerTime / 60000) % 60)).slice(-2);
     let seconds = ('0' + (Math.floor((timerTime / 1000) % 60) % 60)).slice(-2);
@@ -62,20 +58,28 @@ class App extends React.Component {
     return (
       <div>
         <h1>Protect your eyes</h1>
-        <p>
-          According to optometrists in order to save your eyes, you should
-          follow the 20/20/20. It means you should to rest your eyes every 20
-          minutes for 20 seconds by looking more than 20 feet away.
-        </p>
-        <p>
-          This app will help you track your time and inform you when it's time
-          to rest.
-        </p>
-        <img src='./images/work.png' />
-        <img src='./images/rest.png' />
-        <div className='timer'>
-          {minutes} : {seconds}
-        </div>
+        {timerOn === false && (
+          <div>
+            <p>
+              According to optometrists in order to save your eyes, you should
+              follow the 20/20/20. It means you should to rest your eyes every
+              20 minutes for 20 seconds by looking more than 20 feet away.
+            </p>
+            <p>
+              This app will help you track your time and inform you when it's
+              time to rest.
+            </p>
+          </div>
+        )}
+        {timerOn === true && (
+          <div>
+            {timerRest === false && <img src='./images/work.png' />}
+            {timerRest === true && <img src='./images/rest.png' />}
+            <div className='timer'>
+              {minutes} : {seconds}
+            </div>
+          </div>
+        )}
         {timerOn === false && (
           <button className='btn' onClick={this.startTimer}>
             Start
